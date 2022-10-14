4ae60875ab3a0ee7942f3bf2aed7838859d5a29c
Nex <boultoncomputing@Gmail.com>
2022-10-14 17:09:23 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//Begin Connect
+const { Client, GatewayIntentBits, CommandInteraction} = require("discord.js");//Import Discord
+global.config = require('./config/config.json');//Import Config
+
+const client = new Client({
+    fetchAllMembers: true,
+    partials: ["CHANNEL", "MESSAGE"],
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});  //Create Client and announce intents
+
+client.login(config.token)
+    .then(r => client.on("ready", () =>{
+        console.log("I am ready and watching messages!")
+    })); // login using the provided discord token and report ready
+//End Connect
